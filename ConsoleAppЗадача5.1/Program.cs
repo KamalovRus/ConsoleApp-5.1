6572aca55a92744f8f231b1570dcb6631c752b45
KamalovRus <kamrus97@mail.ru>
2021-11-23 19:12:26 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppЗадача5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppЗадача5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] A = new int[7];
+			double Sum = 0;
+			for (int i = 0; i < 7; i++)
+			{
+				A[i] = Convert.ToInt32(Console.ReadLine().ToString());
+				
+				Sum += A[i];
+			}
+			Console.WriteLine("Ans = {0}", Sum / 7);
+			Console.ReadKey();
+		}
+
+    }
+}
